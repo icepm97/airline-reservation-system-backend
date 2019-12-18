b9dbad6a08118d1fdb2676235732d897185c6343
malithbc <47839907+malithbc@users.noreply.github.com>
2019-12-18 20:04:37 +0530

    fix register customer

--- models/customers.js
+++ models/customers.js
@@ -6,127 +6,81 @@ const shortid = require('shortid')
 
 const login = (email, password, result) => {
     connection.query('SELECT `customer_id`, `email`, `password` FROM `customer_login` JOIN `customer` USING (`email`) WHERE email=? LIMIT 1', email, (err, res) => {
-        let data = ((!err) && (res.length == 1) && (res[0].password == password)) ? {id: res[0].customer_id} : null
+        let data = ((!err) && (res.length == 1) && (res[0].password == password)) ? { id: res[0].customer_id } : null
         result(err, data)
     })
 }
 
-const register = (email ,first_name ,last_name ,gender ,birthday ,NIC ,category ,password ,result) => {
-    connection.query('SELECT `email` FROM customer_login` JOIN `customer` USING (`email`) WHERE email=?', email, function(error) {
-        if((!error) && (res.length == 0)) {
+const register = (email, first_name, last_name, gender, birthday, NIC, category, password, result) => {
+    //connection.query('SELECT checkMail(?) AS `count`', email, function(error,res) 
+    connection.query('SELECT checkMail(?) AS `count`', email, function (error, res) {
+        if ((!error) && (res[0].count == 0)) {
             var customer_id = shortid.generate();
-            
-            connection.query('SELECT `customer_id` FROM customer', function(error) {
-                if(error){
-                    throw error;
+
+            connection.query('SELECT `customer_id` FROM customer', function (error, res) {
+                if (error) {
+                    result(error, null)
                 }
-                while(res.includes(customer_id)){
+                while (res.includes(customer_id)) {
                     customer_id = shortid.generate();
                 }
             });
 
-            if (validations(email,password)){
-                connection.beginTransaction(function(err) {
+            if (validations(email, password)) {
+                connection.beginTransaction(function (err) {
                     if (err) { result(err, null) }
-                    connection.query('INSERT into customer_login (`email`,`password`) VALUES (?,?)', (email,password), function (error) {
+                    connection.query('INSERT into customer_login (`email`,`password`) VALUES (?,?)', (email, password), function (error) {
                         if (error) {
-                            return connection.rollback(function() {
-                            throw error;
+                            return connection.rollback(function () {
+                                tresult(error, null)
                             });
-                        }
-                
-                    connection.query('INSERT INTO customer (`customer_id`,`email`,`first_name`,`last_name`,`gender`,`birthday`,`NIC`,`category`) VALUES (?,?,?,?,?,?,?,?)', (customer_id,email,first_name,last_name,gender,birthday,NIC,category), function (error) {
-                        if (error) {
-                        return connection.rollback(function() {
-                            throw error;
-                        });
                         }
 
-                        connection.commit(function(err) {
-                        let data = (!err) ? {id: customer_id} : null
-                        if (err) {
-                            return connection.rollback(function() {
-                            //throw err;
-                            result(err,data)
+                        connection.query('INSERT INTO customer (`customer_id`,`email`,`first_name`,`last_name`,`gender`,`birthday`,`NIC`,`category`) VALUES (?,?,?,?,?,?,?,?)', (customer_id, email, first_name, last_name, gender, birthday, NIC, category), function (error) {
+                            if (error) {
+                                return connection.rollback(function () {
+                                    result(error, null)
+                                });
+                            }
+
+                            connection.commit(function (err) {
+                                let data = { id: customer_id }
+                                if (err) {
+                                    return connection.rollback(function () {
+                                        result(err, null)
+                                    });
+                                }
+                                console.log('success!');
+                                result(null, data)
                             });
-                        }
-                        console.log('success!');
-                        result(err, data)
                         });
-                    });
                     });
                 });
             }
         }
-    }); 
+    });
 }
 
-function validations(email, pass){
-    var schema = new passwordValidator();
+function validations(email, password) {
+    var schema = new schema();
     // Add properties to it
     schema
-    .is().min(8)                                    // Minimum length 8
-    .is().max(100)                                  // Maximum length 100
-    .has().uppercase()                              // Must have uppercase letters
-    .has().lowercase()                              // Must have lowercase letters
-    .has().digits()                                 // Must have digits
-    .has().not().spaces()                           // Should not have spaces
+        .is().min(8)                                    // Minimum length 8
+        .is().max(100)                                  // Maximum length 100
+        .has().uppercase()                              // Must have uppercase letters
+        .has().lowercase()                              // Must have lowercase letters
+        .has().digits()                                 // Must have digits
+        .has().not().spaces()                           // Should not have spaces
     //.is().not().oneOf(['Password123']);             // Blacklist these values
 
-    let mail = (validator.validate(email) ? true: console.log("email format is wrong"))
-    let pass = (schema.validate(pass) ? true: console.log("password must have length 8<= pass<= 100, atleast an upperletter, a lowerletter, a digit and should not have 'space'"))
+    let mail = (validator.validate(email) ? true : console.log("email format is wrong"))
+    let pass = (schema.validate(password) ? true : console.log("password must have length 8<= pass<= 100, atleast an upperletter, a lowerletter, a digit and should not have 'space'"))
 
-    if (mail && pass){
+    if (mail && pass) {
         return true;
     }
     return false;
 }
 
-        //email, validate
 
-        var customer_id = shortid.generate()
-        // con.query('INSERT INTO customer_login (`email`, `password`) values (?,?)', [email, password], (err, res) => {
-        //     let data = ((!err)) ? {id: customer_id} : null
-        //     result(err, data)
-        // })
-
-        // Transaction
-        con.beginTransaction(function(err) {
-            if (err) { throw err; }
-            con.query('INSERT INTO customer_login (`email`, `password`) values (?,?)', [email, password], function (error, res, fields) {
-              if (error) {
-                return con.rollback(function() {
-                  throw error;
-                });
-              }
-          
-            var log = 'Post ' + res.insertId + ' added';
-          
-            con.query('INSERT INTO customer (`customer_id`,`email`,`first_name`,`last_name`,`gender`,`birthday`,`NIC`,`category`) values (?,?,?,?,?,?,?,?)', [customer_id, email, first_name, last_name, gender, birthday, NIC, category], function (error, res, fields) {
-                if (error) {
-                  return con.rollback(function() {
-                    throw error;
-                  });
-                }
-                con.commit(function(err) {
-                  if (err) {
-                    return con.rollback(function() {
-                      throw err;
-                    });
-                  }
-                  console.log('success!');
-                });
-              });
-            });
-            let data = ((!err)) ? {id: customer_id} : null
-            result(err, data)
-          });
-    }
-    else{
-        let err = null
-        let data = {id: '#already_register#'}
-        result(err, data)
-    }
-}
-
-module.exports = {login, register}
+module.exports = { login, register }
