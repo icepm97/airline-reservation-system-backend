ffa2fc78d1a56963019ee3a9419f49a930c1b16d
Vidura Prasangana <vidura.prasangana16@gmail.com>
2019-12-27 17:27:01 +0530

    bug fix

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router()
 router.use('/customers', require('./customers'))
 router.use('/management', require('./management'))
 router.use('/flight',require('./flight'))
+router.use('/schedule',require('./schedule'))
 router.use('/route',require('./route'))
 router.use('/aircraft',require('./aircraft'))
 module.exports = router
