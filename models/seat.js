84740def10b6418b230664a37f42b2df512aeee4
Vidura Prasangana <vidura.prasangana16@gmail.com>
2019-12-29 02:30:01 +0530

    fix

--- models/seat.js
+++ models/seat.js
@@ -1,7 +1,7 @@
 const pool = require('./db')
 
 const getSeats = async (aircraft_model,date) => {
-    let { rows } = await pool.query("select * from seat natural join isbooked(seat.seat_id,1,$1) as booked where aircraft_model = $2", [aircraft_model,date])
+    let { rows } = await pool.query("select * from seat inner join flight on seat.aircraft_model=flight.aircraft_model natural join isbooked(seat.seat_id,flight_id,$1)  as booked where flight_id = $2", [date,aircraft_model])
     return rows;
 }
 module.exports = {getSeats}
