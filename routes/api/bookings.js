8ce5ecc4718d2f4ba836d13a02ec4952c304c7be
Vidura Prasangana <vidura.prasangana16@gmail.com>
2019-12-31 01:40:48 +0530

    fix

--- routes/api/bookings.js
+++ routes/api/bookings.js
@@ -18,7 +18,7 @@ router.get('/:booking_id', middlewareJWT(types.customer), (req, res) => {
 })
 
 
-router.get('/', middlewareJWT(types.customer), (req, res) => {
+router.get('/',middlewareJoi(schemas.jwtGET,"query"), middlewareJWT(types.customer,"query"), (req, res) => {
   booking.getCustomerBookings(req.user_id)
     .then(result => {
       response.data(res, 200, result) 
