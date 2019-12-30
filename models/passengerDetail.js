29b689bdb8fd2f7b99d2a7ff0fab067c77f24d74
malithbc <47839907+malithbc@users.noreply.github.com>
2019-12-30 18:14:28 +0530

    fix passengerDetail

--- models/passengerDetail.js
+++ models/passengerDetail.js
@@ -1,7 +1,7 @@
 const connection = require('./db')
 
 const getPassenger = async (flight_id, date) => {
-    let { rows } = await connection.query('SELECT passenger.passenger_id, passenger.first_name, passenger.last_name, class.class_id, class.name, seat.seat_id, seat.aircraft_model, seat.row, seat.column FROM ticket NATURAL JOIN passenger NATURAL JOIN seat NATURAL JOIN class WHERE ticket.flight_id= $1 AND ticket.date= $2', [flight_id, date])
+    let { rows } = await connection.query('SELECT passenger.passenger_id, passenger.first_name, passenger.last_name, class.class_id, class.name, seat.seat_id, seat.aircraft_model, seat.row, seat.column FROM ticket NATURAL JOIN passenger NATURAL JOIN seat NATURAL JOIN class natural join booking WHERE booking.flight_id= $1 AND booking.date= $2', [flight_id, date])
     if(rows.length!=0){
         let data = rows
         return data
