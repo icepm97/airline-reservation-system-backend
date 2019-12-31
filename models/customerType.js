d3ebb9dda3dca326efc28095b9dc91eef20fd7b4
malithbc <47839907+malithbc@users.noreply.github.com>
2019-12-31 23:14:32 +0530

    fix customerType

--- models/customerType.js
+++ models/customerType.js
@@ -1,7 +1,7 @@
 const connection = require('./db')
 
 const getcustomerType = async (start_date, end_date) => {
-    let { rows } = await connection.query('SELECT DISTINCT customer.category AS customerType, numberOfBooking(customer.category, $1, $2) FROM booking NATURAL JOIN customer WHERE booking.date BETWEEN $1 AND $2', [start_date, end_date])
+    let { rows } = await connection.query('select distinct category, numberofbooking from getpassengertype where booked_date between $1 and $2', [start_date, end_date])
     if(rows.length!=0){
         
         return rows;
