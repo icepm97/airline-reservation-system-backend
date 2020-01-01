67fc9e8899739b73748d18a9c93c87c64737cc52
malithbc <47839907+malithbc@users.noreply.github.com>
2020-01-02 03:38:50 +0530

    fix

--- models/flightBookingDetail.js
+++ models/flightBookingDetail.js
@@ -1,7 +1,7 @@
 const connection = require('./db')
 
 const getBookingDetails = async (flight_id, date) => {
-    let { rows } = await connection.query('SELECT booking_id, customer.first_name, customer.last_name, seat.seat_id, bookingCount(booking_id), seat.aircraft_model, seat.row, seat.column, flight_id FROM booking NATURAL JOIN ticket NATURAL JOIN seat natural join customer WHERE booking.flight_id=$1 AND booking.date=$2', [flight_id, date])
+    let { rows } = await connection.query('SELECT booking_id, customer.first_name, customer.last_name, seat.seat_id, bookingCount(booking_id), seat.aircraft_model, seat.row, seat.column, flight_id FROM schedule booking NATURAL JOIN ticket NATURAL JOIN seat natural join customer JOIN schedule USING (schedule_id) WHERE schedule.flight_id=$1 AND schedule.date=$2', [flight_id, date])
     if(rows.length!=0){
         
         return rows;
--- models/reports/passengerDetail.js
+++ models/reports/passengerDetail.js
@@ -1,7 +1,7 @@
 const connection = require('../db')
 
 const getPassenger = async (flight_id, date) => {
-    let { rows } = await connection.query('SELECT  first_name, last_name, seat_class, seat_row, seat_column, checkage(birthday, $2) AS age FROM givenrangepassengers WHERE flight_id=$1 AND date= $2', [flight_id, date])
+    let { rows } = await connection.query('SELECT  first_name, last_name, seat_class, seat_row, seat_column, checkage(current_date, birthday) AS age FROM givenrangepassengers WHERE flight_id=$1 AND date= $2', [flight_id, date])
     console.log(flight_id, date)
     return rows;   
 }
@@ -9,7 +9,7 @@ const getPassenger = async (flight_id, date) => {
 
 const getRequestedPassengers = async (destination, start_date, end_date) => {
     console.log(destination, start_date, end_date)
-    let { rows } = await connection.query('SELECT * FROM givenrangepassengers WHERE destination_id = $1 AND $2 <= date  AND date <= $3 ', [destination, start_date, end_date])
+    let { rows } = await connection.query('SELECT first_name, last_name,  seat_class, seat_row, seat_column, checkage(current_date, birthday) AS age FROM givenrangepassengers WHERE destination_id = $1 AND $2 <= date  AND date <= $3 ', [destination, start_date, end_date])
     return rows;
 }
 
