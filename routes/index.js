4cff0bef4189bdce4dcb2611351d80f5592f9142
Vidura Prasangana <vidura.prasangana16@gmail.com>
2020-01-01 20:20:11 +0530

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const pool = require("../models/db");
 
 router.get('/', (req, res) => {
-  pool.query("select now()", [], (result, error) => {
+  pool.query("select now()", [], (err, result) => {
 
     res.send(result.rows)
   })
