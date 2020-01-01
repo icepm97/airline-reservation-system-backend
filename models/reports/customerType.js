34e8df1a6fcea0a3cba5a82ef9b0c440212a39a6
RashmikaLakshan <47639124+RashmikaLakshan@users.noreply.github.com>
2020-01-01 13:00:40 +0530

    fix

--- models/reports/customerType.js
+++ models/reports/customerType.js
@@ -1,15 +1,8 @@
-const connection = require('./db')
+const connection = require('../db')
 
 const getcustomerType = async (start_date, end_date) => {
     let { rows } = await connection.query('select distinct category, numberofbooking from getpassengertype where booked_date between $1 and $2', [start_date, end_date])
-    if(rows.length!=0){
-        
-        return rows;
-    }
-    else{
-        return null
-    }
-    
+    return rows;  
 }
 
 module.exports = {getcustomerType}
