33b25a05d26eb5c4f83ff146b0348b700096989a
icepm97 <wgpmihiranga@gmail.com>
2020-01-01 15:25:40 +0530

    getRevenuePerAircraftModel

--- routes/api/reports/revenuePerAircraftModel.js
+++ routes/api/reports/revenuePerAircraftModel.js
@@ -1,7 +1,15 @@
 const router = require('express').Router()
+const response = require('../../../helper/response')
+const model = require('../../../models/reports/revenuePerAircraftModel')
 
 router.get('/', (req, res) => {
-  res.send('revenue')
+  model.getRevenuePerAircraftModel()
+    .then(result => {
+      response.data(res, 200, result)
+    })
+    .catch(error => {
+      response.error(res, 500, 'server_error', 'Server Error', error)
+    })
 })
 
 module.exports = router
