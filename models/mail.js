e9894a4d3c47530d5bf052994ef4759d6a9380f8
icepm97 <wgpmihiranga@gmail.com>
2020-01-02 02:31:48 +0530

    add sendmail

---
+++ models/mail.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+
+const sendMail = async (to, subject, text, html) => {
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, 
+        auth: {
+            user: 'ars.systm.info@gmail.com', 
+            pass: 'goiya@ars' 
+        }
+    });
+
+
+    let info = await transporter.sendMail({
+        from: 'ars.systm.info@gmail.com', 
+        to, 
+        subject, 
+        text, 
+        html 
+    });
+
+    console.log(info);
+}
